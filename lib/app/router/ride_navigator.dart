dc050a820105dfd046ec83324a6a630f8f8d769f
baccarisverige-ux <baccari.sverige@gmail.com>
2026-09-12 06:19:30 +0000

    Fix RideNavigator route type for analyzer

--- lib/app/router/ride_navigator.dart
+++ lib/app/router/ride_navigator.dart
@@ -1,14 +1,14 @@
-
 import 'package:flutter/material.dart';
 import 'package:movera_rider/app/router/routes.dart';
-import 'package:movera_rider/shared/widgets/navigation_transition.dart';
 
-/// Single navigation API. Screens should not mix Get.to and raw pushes.
 abstract final class RideNavigator {
   static Future<T?> open<T>(BuildContext context, Widget page, {String? name}) {
     return Navigator.push<T>(
       context,
-      BottomToTopTransition(page),
+      MaterialPageRoute<T>(
+        settings: RouteSettings(name: name),
+        builder: (_) => page,
+      ),
     );
   }
 
