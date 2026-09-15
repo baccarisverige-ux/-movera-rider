1da6400d47a26a1194fcd77735c91b1d649238fc
baccarisverige-ux <baccari.sverige@gmail.com>
2026-09-15 04:34:50 +0200

    Remove unused History import

--- lib/features/history/presentation/ride_history.dart
+++ lib/features/history/presentation/ride_history.dart
@@ -4,7 +4,6 @@ import 'package:movera_rider/app/di.dart';
 import 'package:movera_rider/features/history/application/on_demand_history_controller.dart';
 import 'package:movera_rider/features/reservations/application/reservation_controller.dart';
 import 'package:movera_rider/features/reservations/domain/reservation.dart';
-import 'package:movera_rider/features/reservations/domain/reservation_status.dart';
 import 'package:movera_rider/features/reservations/presentation/reservation_format.dart';
 import 'package:movera_rider/features/reservations/presentation/reservation_widgets.dart';
 import 'package:movera_rider/features/reservations/presentation/upcoming_reservation.dart';
