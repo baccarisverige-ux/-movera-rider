75ed6e0c4a85f970dc36388f8b960b727941488c
baccarisverige-ux <baccari.sverige@gmail.com>
2026-09-15 07:08:38 +0200

    Remove seeded completed-trip receipt data

--- lib/features/ride_booking/data/driver_repository.dart
+++ lib/features/ride_booking/data/driver_repository.dart
@@ -45,10 +45,9 @@ class TripReceipt {
 }
 
 class TripReceiptRepository {
-  TripReceipt last() => const TripReceipt(
-    pickup: 'I11/Street 15 - h350',
-    destination: 'Skypulse solution',
-    total: '\$10.12',
-    method: 'Cash',
-  );
+  const TripReceiptRepository({TripReceipt? last}) : _last = last;
+
+  final TripReceipt? _last;
+
+  TripReceipt? last() => _last;
 }
