84581d59aab8e26ffd28271bad3285d499b153eb
baccarisverige-ux <baccari.sverige@gmail.com>
2026-09-15 04:15:05 +0200

    Split history persistence out of ride completion

--- lib/features/history/application/on_demand_ride_history_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:movera_rider/features/history/data/on_demand_ride_history_store.dart';
-import 'package:movera_rider/features/reservations/domain/reservation.dart';
-
-class OnDemandRideHistoryController {
-  const OnDemandRideHistoryController();
-
-  Future<List<Reservation>> load() => OnDemandRideHistoryStore.read();
-}
