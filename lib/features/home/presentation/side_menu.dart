0f4c9791f67015a91610b971a879bd17f919d2d0
baccarisverige-ux <baccari.sverige@gmail.com>
2026-09-22 07:35:02 +0200

    Rider: keep Scheduled Rides out of side menu

--- lib/features/home/presentation/side_menu.dart
+++ lib/features/home/presentation/side_menu.dart
@@ -4,7 +4,6 @@ import 'package:movera_rider/core/constants/appassets.dart';
 import 'package:movera_rider/core/constants/appfontweight.dart';
 import 'package:movera_rider/features/wallet/presentation/wallet.dart';
 import 'package:movera_rider/features/history/presentation/ride_history.dart';
-import 'package:movera_rider/features/scheduled_rides/presentation/scheduled_rides.dart';
 import 'package:movera_rider/features/notifications/presentation/notifications.dart';
 import 'package:movera_rider/features/messages/presentation/messages.dart';
 import 'package:movera_rider/features/saved_places/presentation/saved_places.dart';
@@ -196,12 +195,6 @@ class RiderSideMenu extends StatelessWidget {
             image: null,
             title: 'Ride History',
             onTap: () => _pushPage(context, RideHistory()),
-          ),
-          (
-            icon: Icons.event_available_outlined,
-            image: null,
-            title: 'Scheduled Rides',
-            onTap: () => _pushPage(context, const ScheduledRides()),
           ),
           (
             icon: Icons.credit_card_outlined,
