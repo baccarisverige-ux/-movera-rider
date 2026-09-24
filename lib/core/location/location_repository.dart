18b6f893f2ab836f19b82345f6ac787f11f7e9e2
baccarisverige-ux <baccari.sverige@gmail.com>
2026-09-24 23:46:32 +0200

    testability(batch4): expose location repository seam

--- lib/core/location/location_repository.dart
+++ lib/core/location/location_repository.dart
@@ -1,19 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:geolocator/geolocator.dart';
 
 /// Screens talk to this, never to Geolocator.
 class LocationRepository {
+  const LocationRepository();
+  @visibleForTesting
   Future<bool> isLocationServiceEnabled() {
     return Geolocator.isLocationServiceEnabled();
   }
 
+  @visibleForTesting
   Future<LocationPermission> checkPermission() {
     return Geolocator.checkPermission();
   }
 
+  @visibleForTesting
   Future<LocationPermission> requestPermission() {
     return Geolocator.requestPermission();
   }
 
+  @visibleForTesting
   Future<Position> getCurrentPosition({LocationSettings? locationSettings}) {
     return Geolocator.getCurrentPosition(
       locationSettings: locationSettings ??
@@ -21,10 +27,12 @@ class LocationRepository {
     );
   }
 
+  @visibleForTesting
   Stream<Position> getPositionStream({LocationSettings? locationSettings}) {
     return Geolocator.getPositionStream(locationSettings: locationSettings);
   }
 
+  @visibleForTesting
   double distanceBetween(
     double startLatitude,
     double startLongitude,
