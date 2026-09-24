d39ba1bb8e33062d3f326a2a9e5e451832b5f91e
baccarisverige-ux <baccari.sverige@gmail.com>
2026-09-24 06:17:47 +0200

    Phase 51: use Wasm-compatible web conditional imports

--- lib/core/web/web_standalone.dart
+++ lib/core/web/web_standalone.dart
@@ -1,5 +1,5 @@
 import 'package:movera_rider/core/web/web_standalone_io.dart'
-    if (dart.library.html) 'package:movera_rider/core/web/web_standalone_web.dart';
+    if (dart.library.js_interop) 'package:movera_rider/core/web/web_standalone_web.dart';
 
 /// True when the app is running as an installed PWA, not a browser tab.
 bool isInstalledWebApp() => webIsInstalledApp();
