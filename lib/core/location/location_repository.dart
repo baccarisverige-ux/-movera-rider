48e6177e93d8ca68c418a05c0e062fbd1e301a95
baccarisverige-ux <baccari.sverige@gmail.com>
2026-09-24 23:46:38 +0200

    testability(batch4): keep overridable location repository seam

--- lib/core/location/location_repository.dart
+++ lib/core/location/location_repository.dart
@@ -1,25 +1,20 @@
-import 'package:flutter/foundation.dart';
 import 'package:geolocator/geolocator.dart';
 
 /// Screens talk to this, never to Geolocator.
 class LocationRepository {
   const LocationRepository();
-  @visibleForTesting
   Future<bool> isLocationServiceEnabled() {
     return Geolocator.isLocationServiceEnabled();
   }
 
-  @visibleForTesting
   Future<LocationPermission> checkPermission() {
     return Geolocator.checkPermission();
   }
 
-  @visibleForTesting
   Future<LocationPermission> requestPermission() {
     return Geolocator.requestPermission();
   }
 
-  @visibleForTesting
   Future<Position> getCurrentPosition({LocationSettings? locationSettings}) {
     return Geolocator.getCurrentPosition(
       locationSettings: locationSettings ??
@@ -27,12 +22,10 @@ class LocationRepository {
     );
   }
 
-  @visibleForTesting
   Stream<Position> getPositionStream({LocationSettings? locationSettings}) {
     return Geolocator.getPositionStream(locationSettings: locationSettings);
   }
 
-  @visibleForTesting
   double distanceBetween(
     double startLatitude,
     double startLongitude,
